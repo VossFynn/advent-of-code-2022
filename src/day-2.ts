4046acabd0226ae5a09dc3be175ecdaf7032ca46
Fynn Luca Voß <fynn-luca.voss@lhind.dlh.de>
2022-12-02 15:12:10 +0100

    added part 2 as a seperate funciton

--- src/day-2.ts
+++ src/day-2.ts
@@ -12,19 +12,80 @@ function getInputData(day: string): Array<Array<string>> {
 
 const inputDayTwo = getInputData('2');
 
+/**
+ * This enum stores the relevant information about how much points u get for each action
+ */
 enum ScoreEnum {
   WIN = 6,
   DRAW = 3,
   LOSE = 0
 }
 
+/**
+ * This enum stores the relevant information about how much points u get for each shape
+ */
 enum ShapeEnum {
   ROCK = 1,
   PAPER = 2,
   SCISSOR = 3
 }
 
-function calculateScore(strategy: string[]): number {
+/**
+ * Calculating the score for part one of advent of code day 1
+ * In this part the score is calculated based on the giving string pair
+ * Each string represents a move, like Rock Paper or Scissors
+ *
+ * @param strategy {string[]}
+ *
+ * @return {number} the calculated score
+ */
+function calculateScorePartOne(strategy: string[]): number {
+  switch (strategy[1]) {
+    case 'X':
+      switch (strategy[0]) {
+        case 'A':
+          return ScoreEnum.DRAW + ShapeEnum.ROCK;
+        case 'B':
+          return ScoreEnum.LOSE + ShapeEnum.ROCK;
+        default:
+          return ScoreEnum.WIN + ShapeEnum.ROCK;
+      }
+    case 'Y':
+      switch (strategy[0]) {
+        case 'A':
+          return ScoreEnum.WIN + ShapeEnum.PAPER;
+        case 'B':
+          return ScoreEnum.DRAW + ShapeEnum.PAPER;
+        default:
+          return ScoreEnum.LOSE + ShapeEnum.PAPER;
+      }
+    case 'Z':
+      switch (strategy[0]) {
+        case 'A':
+          return ScoreEnum.LOSE + ShapeEnum.SCISSOR;
+        case 'B':
+          return ScoreEnum.WIN + ShapeEnum.SCISSOR;
+        default:
+          return ScoreEnum.DRAW + ShapeEnum.SCISSOR;
+      }
+    default:
+      return 0
+  }
+}
+
+/**
+ * Calculating the score for part two of advent of code day 2
+ * In this part the score is calculated based on what we should achieve
+ * We should only WIN, DRAW or LOOSE.
+ *
+ * The first string represents a move, like Rock Paper or Scissors
+ * The second string represents the outcome, like WIN, DRAW or LOOSE
+ *
+ * @param strategy {string[]}
+ *
+ * @return {number} the calculated score
+ */
+function calculateScorePartTwo(strategy: string[]): number {
   switch (strategy[1]) {
     case 'X':
       switch (strategy[0]) {
@@ -59,9 +120,12 @@ function calculateScore(strategy: string[]): number {
 }
 
 function dayTwo(): void {
-  let score: number = 0;
-  _.chunk(inputDayTwo, 2).map((entry: string[]) => score += calculateScore(entry));
-  console.log("The Score is approximately: " + score)
+  let scorePartOne: number = 0;
+  let scorePartTwo: number = 0;
+  _.chunk(inputDayTwo, 2).map((entry: string[]) => scorePartOne += calculateScorePartOne(entry));
+  _.chunk(inputDayTwo, 2).map((entry: string[]) => scorePartTwo += calculateScorePartTwo(entry));
+  console.log("The Score in part 1 is approximately: " + scorePartOne)
+  console.log("The Score in part 2 is approximately: " + scorePartTwo)
 }
 
 dayTwo();
